1995785505aded7587b3b278a681f3ab87099fb6
CstoneKu <162235645+CstoneKu@users.noreply.github.com>
2024-07-02 16:04:37 +0900

    feat:dockerfile, yaml 추가

--- src/main/java/com/sh/app/AppController.java
+++ src/main/java/com/sh/app/AppController.java
@@ -1,11 +1,13 @@
 package com.sh.app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class AppController {
 
     @Value("${app.version}")
@@ -14,6 +16,7 @@ public class AppController {
     @GetMapping
     @ResponseBody   //리턴한 객체를 바로 응답 메시지에 써달라
     public String index(){
+        log.info("GET / : app version : {}", appVersion);
         return "Welcome to Hello world application 🦄 : version " + appVersion;
     }
 }
